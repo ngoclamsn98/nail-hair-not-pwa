08e187d567e2166354ff5b9e3bcbebe0bf67a765
ngoclamsn98 <ngoclamdk43@gmail.com>
2024-06-23 21:46:47 +0700

    fix: input

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <router-view />
-  <vue3-confirm-dialog />
 </template>
 
 <script setup>
-import { getCurrentInstance, watch } from "vue";
-import { useStore } from "vuex";
-
-const store = useStore();
-
-const instance = getCurrentInstance();
-const app = instance.appContext.app;
 </script>
