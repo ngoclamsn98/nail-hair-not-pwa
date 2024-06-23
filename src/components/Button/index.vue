cf275041a6b11ae3c46deaa6567d7a32590a1250
ngoclamsn98 <ngoclamdk43@gmail.com>
2024-06-23 21:32:24 +0700

    fix: input

---
+++ src/components/Button/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="w-full justify-center flex"
+    v-if="false"
+  >
+    <div class="w-[45px]">
+      <Loading />
+    </div>
+  </div>
+  <button
+    v-else
+    v-wave="{ color: 'blue' }"
+    class="flex gap-x-[5px] px-[10px] h-[45px] items-center rounded-lg border border-gray-200 min-w-[100px] justify-center bg-button text-white"
+  >
+    <slot name="startButton" />
+    <slot name="text" />
+    <slot name="endButton" />
+  </button>
+</template>
+
+<script setup>
+import Loading from "@/components/Loading";
+import { useStore } from "vuex";
+defineProps({
+  isLoading: {
+    type: Boolean,
+    default: true,
+  },
+});
+const store = useStore();
+</script>
--- src/modules/Login/index.vue
+++ src/modules/Login/index.vue
@@ -1,29 +1,32 @@
 <template>
-  <div>
-    <input
-      class="border border-gray-100"
-      placeholder="username"
-      v-model="user.username"
-    />
-    <input
-      class="border border-gray-100"
-      placeholder="password"
-      type="password"
-      v-model="user.password"
-    />
-    <button @click="submitForm">submit</button>
+  <div class="w-full relative h-full flex items-center bg-content flex-col">
+    <form class="gap-y-[10px] flex flex-col w-[80%] m-auto justify-between">
+      <h2 class="text-[32px] mb-[20px]">LITI <br />Nail & Hair</h2>
+      <div class="flex flex-col justify-end">
+        <InputText
+          name="username"
+          placeholder="Please enter username"
+          class="mt-0"
+        />
+        <InputText
+          name="password"
+          type="password"
+          placeholder="Please enter password"
+          class="!mt-0"
+        />
+        <div class="w-full">
+          <Button class="mb-[20px] w-full border-0">
+            <template v-slot:text>Login</template>
+          </Button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
-
+  
 <script setup>
-import { ref } from "vue";
+import Button from "@/components/Button";
 
-const user = ref({
-  username: "",
-  password: "",
-});
-const submitForm = (e) => {
-  console.log(user);
-  e.preventDefault();
-};
+import InputText from "@/components/InputText";
 </script>
+  
