350dc55fdcbbc036d3492be870631ac916e17840
ngoclamsn98 <ngoclamdk43@gmail.com>
2024-06-30 10:15:18 +0700

    fix: report

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,4 +1,6 @@
-export const numberWithCommas = (number: Number) => {
+export const numberWithCommas = (number: String) => {
+  console.log(number, "numbernumber");
+
   if (!number) return 0;
   const regex = new RegExp(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g);
   return number.toString().replace(regex, ",");
